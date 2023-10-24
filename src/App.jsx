1b13c407c7b6670b2036ba973e0492cc32201731
Amábile <amabile.analistadesistemas@gmail.com>
2023-10-24 13:41:45 -0300

    calculadora terminada

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import HelloWord from './componentes/HelloWord';
+
+function App() {
+  return (
+    <div className='App'>
+    <HelloWord/>
+    </div>
+  );
+}
+export default App;
---
+++ src/componentes/HelloWord.jsx
@@ -0,0 +1,10 @@
+export default function(props) {
+  return (
+    <div className='App'>
+      {/* h1 é um componente */}
+      <h1>Hello Word! {props className="title"></h1> 
+     </div>
+  );
+}
+
+
